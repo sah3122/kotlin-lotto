d5a4396d64c516d8d212b179e757276a42849114
dongchul <sorkehdcjf@gmail.com>
2022-05-30 22:27:27 +0900

    feat: PurchaseFactory -> Static factory method  변경

--- src/main/kotlin/lotto/application/vo/Purchase.kt
+++ src/main/kotlin/lotto/application/vo/Purchase.kt
@@ -1,6 +1,23 @@
 package lotto.application.vo
 
-data class Purchase(
+private const val LOTTO_PRICE = 1000
+
+data class Purchase private constructor(
     val amount: Amount,
     val purchaseCounts: PurchaseCounts
-)
+) {
+    companion object {
+        fun create(
+            amount: Long,
+            manualLottoCount: Int
+        ): Purchase {
+            require(manualLottoCount * LOTTO_PRICE <= amount) { "수동 로또 갯수가 구입 금액보다 클수 없습니다." }
+
+            val autoLottoCount: Int = ((amount - manualLottoCount * LOTTO_PRICE) / LOTTO_PRICE).toInt()
+            val purchaseCounts = PurchaseCounts(PurchaseCount(autoLottoCount), PurchaseCount(manualLottoCount))
+            val amount = Amount(amount)
+
+            return Purchase(amount, purchaseCounts)
+        }
+    }
+}
